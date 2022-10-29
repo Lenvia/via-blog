201c7104eeff43455a462d882ba4cb249c874a37
Lenvia <yrp1199@163.com>
2022-10-29 16:45:01 +0800

    add DAO of user&category and controller outline of models

---
+++ api/v1/category.go
@@ -0,0 +1,28 @@
+package v1
+
+import "github.com/gin-gonic/gin"
+
+// AddCategory 添加分类
+func AddCategory()  {
+
+}
+
+// GetCategory 查询分类信息
+func GetCategory(c *gin.Context)  {
+
+}
+
+// GetCategories 查询分类列表
+func GetCategories(c *gin.Context)  {
+
+}
+
+// EditCategory 编辑分类
+func EditCategory(c *gin.Context)  {
+
+}
+
+// DeleteCategory 删除分类
+func DeleteCategory()  {
+
+}
---
+++ api/v1/user.go
@@ -0,0 +1,33 @@
+package v1
+
+import "github.com/gin-gonic/gin"
+
+// AddUser 添加用户
+func AddUser(c *gin.Context) {
+
+}
+
+// GetUser 查询单个用户
+func GetUser(c *gin.Context) {
+
+}
+
+// GetUsers 查询用户列表
+func GetUsers(c *gin.Context) {
+
+}
+
+// UpdateUser 更新用户
+func UpdateUser(c *gin.Context)  {
+
+}
+
+// ChangeUserPassWord 修改密码
+func ChangeUserPassWord(c *gin.Context)  {
+
+}
+
+// DeleteUser 删除用户
+func DeleteUser(c *gin.Context)  {
+
+}
